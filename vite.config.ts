fb5c47a94fcea7a4fefd6c0f29e5a75d6aa27009
harrazmasri <harraz@rekasawang.com>
2024-04-09 05:59:26 +0800

    vite update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/harrazmasri/', // use this to set base url to repo, github pages requirement
   plugins: [vue()],
 })
